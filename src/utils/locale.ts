1ba967c6eea77a90b77248fd5433840c0fde0521
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-07-12 13:46:02 +0800

    fix utils/locale

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -1,14 +1,13 @@
 import * as locale from 'browser-locale';
-const storage = window.localStorage;
 
 export function getLocale(): Lang {
   return parseLocale();
 }
 
-export const saveLocale = (lang: Lang) => storage.setItem('lang', lang);
+export const saveLocale = (lang: Lang) => window.localStorage.setItem('lang', lang);
 
 function parseLocale (): Lang {
-  const s = storage.getItem('lang') || (() => locale())();
+  const s = window.localStorage.getItem('lang') || (() => locale())();
 
   switch (s.toLowerCase().substr(0,2)) {
     case 'en': return 'en';
